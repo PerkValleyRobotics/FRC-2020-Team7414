278014b8035eb753428670a451b74e2b46f87569
PerkValleyRobotics <perkiomenvalleyrobotics@gmail.com>
2020-01-21 19:26:00 -0500

    Limelight and Nav-X!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,19 +7,31 @@
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import frc.robot.OIHandler;
 import frc.robot.Subsystems.*;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.*;
+import frc.robot.Subsystems.Vision;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.SerialPort;
+
 
 public class Robot extends TimedRobot {
+
+  AHRS ahrs;
+  
 
   public static OIHandler oi;
   public static DriveTrain Gavin;// DriveTrain
   public static Shooter shooter;
   //public static Intake intake;
   public static int time;
+  public static Vision limelight;
 
   @Override
   public void robotInit() {
@@ -27,6 +39,9 @@ public class Robot extends TimedRobot {
     Gavin = new DriveTrain();
     //intake = new Intake();
     oi = new OIHandler();
+    limelight = new Vision();
+    ahrs = new AHRS();
+    //ahrs.enableLogging(true);
   }
 
   @Override
@@ -37,19 +52,63 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
-
+    limelight.updateLimelight();
     SmartDashboard.putNumber("Flywheel RPM:", oi.getRPM());
+    SmartDashboard.putBoolean("IMU Connected? ", ahrs.isConnected());
+    SmartDashboard.putNumber("Yaw: ", ahrs.getYaw());
+    SmartDashboard.putNumber("Pitch: ", ahrs.getPitch());
+    SmartDashboard.putNumber("Roll: ", ahrs.getRoll());
+    SmartDashboard.putNumber("X Acceleration: ", ahrs.getWorldLinearAccelX());
+    SmartDashboard.putNumber("Y Acceleration: ", ahrs.getWorldLinearAccelY());
+    SmartDashboard.putNumber("Z Acceleration: ", ahrs.getWorldLinearAccelZ());
+    SmartDashboard.putNumber("Velocity_X", ahrs.getVelocityX());
+    SmartDashboard.putNumber("Velocity_Y", ahrs.getVelocityY());
+    SmartDashboard.putNumber("Displacement_X", ahrs.getDisplacementX());
+    SmartDashboard.putNumber("Displacement_Y", ahrs.getDisplacementY());
+    SmartDashboard.putNumber("RawGyro_X", ahrs.getRawGyroX());
+    SmartDashboard.putNumber("RawGyro_Y", ahrs.getRawGyroY());
+    SmartDashboard.putNumber("RawGyro_Z", ahrs.getRawGyroZ());
+    SmartDashboard.putNumber("RawAccel_X", ahrs.getRawAccelX());
+    SmartDashboard.putNumber("RawAccel_Y", ahrs.getRawAccelY());
+    SmartDashboard.putNumber("RawAccel_Z", ahrs.getRawAccelZ());
+    SmartDashboard.putNumber("RawMag_X", ahrs.getRawMagX());
+    SmartDashboard.putNumber("RawMag_Y", ahrs.getRawMagY());
+    SmartDashboard.putNumber("RawMag_Z", ahrs.getRawMagZ());
+    SmartDashboard.putNumber("IMU_Temp_C", ahrs.getTempC());
   }
 
   @Override
   public void autonomousPeriodic() {
-    Gavin.diffDrive.arcadeDrive(0.2, 0);
-    try {
-      Thread.sleep(200);
-    } catch (InterruptedException e) {
-      //TODOAuto-generatedcatchblock
-      e.printStackTrace();
-    }
+    limelight.updateLimelight();
+    //Gavin.diffDrive.arcadeDrive(0.2, 0);
+    // try {
+    //   Thread.sleep(200);
+    // } catch (InterruptedException e) {
+    //   //TODOAuto-generatedcatchblock
+    //   e.printStackTrace();
+    // }
     Gavin.diffDrive.arcadeDrive(0, 0);
+    SmartDashboard.putNumber("Flywheel RPM:", oi.getRPM());
+    SmartDashboard.putBoolean("IMU Connected? ", ahrs.isConnected());
+    SmartDashboard.putNumber("Yaw: ", ahrs.getYaw());
+    SmartDashboard.putNumber("Pitch: ", ahrs.getPitch());
+    SmartDashboard.putNumber("Roll: ", ahrs.getRoll());
+    SmartDashboard.putNumber("X Acceleration: ", ahrs.getWorldLinearAccelX());
+    SmartDashboard.putNumber("Y Acceleration: ", ahrs.getWorldLinearAccelY());
+    SmartDashboard.putNumber("Z Acceleration: ", ahrs.getWorldLinearAccelZ());
+    SmartDashboard.putNumber("Velocity_X", ahrs.getVelocityX());
+    SmartDashboard.putNumber("Velocity_Y", ahrs.getVelocityY());
+    SmartDashboard.putNumber("Displacement_X", ahrs.getDisplacementX());
+    SmartDashboard.putNumber("Displacement_Y", ahrs.getDisplacementY());
+    SmartDashboard.putNumber("RawGyro_X", ahrs.getRawGyroX());
+    SmartDashboard.putNumber("RawGyro_Y", ahrs.getRawGyroY());
+    SmartDashboard.putNumber("RawGyro_Z", ahrs.getRawGyroZ());
+    SmartDashboard.putNumber("RawAccel_X", ahrs.getRawAccelX());
+    SmartDashboard.putNumber("RawAccel_Y", ahrs.getRawAccelY());
+    SmartDashboard.putNumber("RawAccel_Z", ahrs.getRawAccelZ());
+    SmartDashboard.putNumber("RawMag_X", ahrs.getRawMagX());
+    SmartDashboard.putNumber("RawMag_Y", ahrs.getRawMagY());
+    SmartDashboard.putNumber("RawMag_Z", ahrs.getRawMagZ());
+    SmartDashboard.putNumber("IMU_Temp_C", ahrs.getTempC());
   }
 }
---
+++ src/main/java/frc/robot/Subsystems/Vision.java
@@ -0,0 +1,44 @@
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class Vision {
+
+NetworkTableEntry tv;
+NetworkTableEntry ta;
+NetworkTableEntry tx;
+NetworkTableEntry ty;
+boolean inRange;
+
+public Vision () {
+    
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    tx = table.getEntry("tx");
+    ty = table.getEntry("ty");
+    ta = table.getEntry("ta");
+    tv = table.getEntry("tv");
+}
+
+public void updateLimelight() {
+    //read values periodically
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double v = tv.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+        if (v < 1.0) {
+        inRange = false;
+        } else {
+        inRange = true;
+        }
+
+    //post to smart dashboard periodically
+    SmartDashboard.putNumber("LimelightX: ", x);
+    SmartDashboard.putNumber("LimelightY: ", y);
+    SmartDashboard.putNumber("In Frame? ", v);
+    SmartDashboard.putBoolean("Detected? ", inRange);
+    SmartDashboard.putNumber("LimelightArea: ", area);
+    }
+}
